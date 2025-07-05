efb915322529ae1221880b1e3641a52be775a7c4
Joshua Carpenter <mtnbikerjoshua@gmail.com>
2025-07-05 15:44:33 -0400

    Slice all the way to left edge if table does not have a left border

--- code/extract_cells.py
+++ code/extract_cells.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import re
 import pandas as pd
+import warnings
 
 def show_wait_destroy(winname, img):
     cv2.imshow(winname, img)
@@ -21,8 +22,7 @@ template_matches = pd.read_csv("output/template_matching.csv")
 template_index = template_matches[(template_matches["legajo"] == legajo) & 
                                   (template_matches["page"] == int(page))]["template_index"].iloc[0]
 
-def load_template(template_name):
-    template_path = f'data/Templates/{template_name}.png'
+def load_template(template_path):
     template_grayscale = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
     _, template = cv2.threshold(template_grayscale, 0, 255, cv2.THRESH_BINARY)
 
@@ -33,7 +33,61 @@ def load_template(template_name):
     template_resized = cv2.resize(template_grayscale, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
     return template_resized
 
-template = load_template(f"blank_template_{template_index + 1}")
+def has_left_line(image_file):
+    # Load and threshold the image
+    image = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
+    if image is None:
+        warnings.warn(f"{image_file} is not readable")
+        return None
+
+    width = 1000
+    scale = 1000/image.shape[1]
+    height = round(scale * image.shape[0])
+
+    image = cv2.resize(image, (width, height))
+    blurred = cv2.GaussianBlur(image, (5, 5), 0)
+    thresholded = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 2)
+
+
+    # Use morphological operations to find horizontal and vertical lines
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 1))
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 50))
+
+    horizontal_lines = cv2.morphologyEx(thresholded, cv2.MORPH_OPEN, horizontal_kernel)
+    vertical_lines = cv2.morphologyEx(thresholded, cv2.MORPH_OPEN, vertical_kernel)
+
+    intersections = cv2.bitwise_and(horizontal_lines, vertical_lines)
+
+    # Get a list of (x, y) intersection coordinates
+    def findCentroids(image):
+        contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        centroids = []
+        for cnt in contours:
+            M = cv2.moments(cnt)
+            if M["m00"] != 0:
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+                centroids.append((cx, cy))
+            else:
+                centroids.append((0, 0))
+        return centroids
+    
+    template = load_template(f"data/Templates/blank_template_{template_index + 1}.png")
+    template_contours, _ = cv2.findContours(template, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    largest_contour = max(template_contours, key = cv2.contourArea)
+    left_line_x, _, _, _ = cv2.boundingRect(largest_contour)
+
+    left_line_centroids = [pt for pt in findCentroids(intersections) if pt[0] < left_line_x + 10 and pt[0] > left_line_x - 10]
+
+    if(len(left_line_centroids) < 7):
+        return False
+    else: 
+        return True
+    
+template = load_template(f"data/Templates/blank_template_{template_index + 1}.png")
+if not has_left_line(image_file):
+    template = load_template(f"data/Slicing Templates/slicing_template_{template_index + 1}.png")
+
 skeleton = cv2.ximgproc.thinning(template)
 contours, hierarchy = cv2.findContours(skeleton, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -50,5 +104,4 @@ for rect in rects:
     x, y, w, h = rect
     cropped_image = image[y:y+h, x:x+w]
     cropped_images.append(cropped_image)
-
-cv2.imwrite("output/test_image.png", cropped_images[-21])
+    cv2.imwrite(f"output/test_images/test_image_{x}_{y}.png", cropped_image)
